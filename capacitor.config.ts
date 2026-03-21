82d00acb17083b346050439bf363da445493cf72
kylechor <kylechor@qq.com>
2026-03-21 15:49:58 +0800

    Add ./capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.growpoints.app',
+  appName: '成长积分宝',
+  webDir: 'dist'
+};
+
+export default config;
